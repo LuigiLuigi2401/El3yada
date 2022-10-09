32bd32db5c896e4f3a39697c3dacbaf015cf1ab9
LuigiLuigi2401 <72325407+LuigiLuigi2401@users.noreply.github.com>
2022-10-09 18:04:54 +0200

    Server Side Forms

--- El3yada/login/views.py
+++ El3yada/login/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
+
+class NameForm(forms.Form):
+    username = forms.CharField(label="",max_length=100,widget=forms.TextInput(attrs={'class': "form-control","id" : "username"}))
+class PasswordForm(forms.Form):
+    password = forms.CharField(label="",max_length=20,widget=forms.PasswordInput(attrs={'class': "form-control","id" : "password"}))
 
 # Create your views here.
 def index(request):
-    return render(request,"login/index.html")
+    username = NameForm()
+    password = PasswordForm()
+    return render(request,"login/index.html",{'form': username,'form2':password})
