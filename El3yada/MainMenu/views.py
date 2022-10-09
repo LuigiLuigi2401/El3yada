df1d29a10917b672aa9b66a918870e58c77c4fc8
LuigiLuigi2401 <72325407+LuigiLuigi2401@users.noreply.github.com>
2022-10-09 22:24:33 +0200

    Add POST to Form

--- El3yada/MainMenu/views.py
+++ El3yada/MainMenu/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        post = request.POST
+        return render(request,"MainMenu/index.html",{"username":post["username"]})
     return render(request,"MainMenu/index.html")
